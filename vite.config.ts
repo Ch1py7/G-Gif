9feb1c11880cc63e998e789f298c21582f9e647d
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-06-16 12:16:33 -0600

    chore: add meta data and new packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import * as path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	root: './',
-	publicDir: './public',
-	resolve: {
-		alias: {
-			assets: path.resolve(__dirname, './src/assets'),
-			components: path.resolve(__dirname, './src/components'),
-			types: path.resolve(__dirname, './src/types'),
-			services: path.resolve(__dirname, './src/services'),
-			context: path.resolve(__dirname, './src/secontextvices'),
-		},
-	},
-	plugins: [react()],
+  root: './',
+  publicDir: './public',
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, './src/assets'),
+      components: path.resolve(__dirname, './src/components'),
+      types: path.resolve(__dirname, './src/types'),
+      services: path.resolve(__dirname, './src/services'),
+      context: path.resolve(__dirname, './src/context'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+    },
+  },
+  plugins: [react()],
 })
